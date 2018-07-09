e802cfa76c77c651fed670ebf2a1f92740cdc49e
Jordan Kicklighter <jwkicklighter@gmail.com>
2018-07-08 22:14:37 -0400

    Add default scss config
    Import a common scss file to every component for variables/mixins.
    Use applications.scss file for global files.
    Change the index.vue template to not include the logo.

--- template/nuxt.config.js
+++ template/nuxt.config.js
@@ -1,7 +1,21 @@
+const resolve = require('path').resolve
+
+const isVueRule = (rule) => {
+  return rule.test.toString() === '/\\.vue$/'
+}
+const isSASSRule = (rule) => {
+  return ['/\\.sass$/', '/\\.scss$/'].indexOf(rule.test.toString()) !== -1
+}
+const sassResourcesLoader = {
+  loader: 'sass-resources-loader',
+  options: {
+    resources: [
+      resolve(__dirname, 'assets/scss/variables.scss'),
+    ],
+  },
+}
+
 module.exports = {
-  /*
-  ** Headers of the page
-  */
   head: {
     title: '{{ name }}',
     meta: [
@@ -10,21 +24,31 @@ module.exports = {
       { hid: 'description', name: 'description', content: '{{escape description }}' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
     ]
   },
-  /*
-  ** Customize the progress bar color
-  */
   loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
+  css: [
+    { src: './assets/scss/application.scss', lang: 'scss' },
+  ],
   build: {
-    /*
-    ** Run ESLint on save
-    */
     extend (config, { isDev, isClient }) {
+      /*
+      ** Inject SCSS variables into components
+      */
+      config.module.rules.forEach((rule) => {
+        if (isVueRule(rule)) {
+          rule.options.loaders.sass.push(sassResourcesLoader)
+          rule.options.loaders.scss.push(sassResourcesLoader)
+        }
+        if (isSASSRule(rule)) {
+          rule.use.push(sassResourcesLoader)
+        }
+      })
+
+      /*
+      ** Run ESLint on save
+      */
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
@@ -33,6 +57,6 @@ module.exports = {
           exclude: /(node_modules)/
         })
       }
-    }
-  }
+    },
+  },
 }
